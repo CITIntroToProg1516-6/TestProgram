b96327d49cdf750cc2c9536a320afd06562ee096
hcps-mcenhimlr <hcps-mcenhimlr@180-18MCENHIMLR.hcps.internal>
2016-04-08 14:58:45 -0400

    Made it print Hello World.

--- src/firsttest/FirstTest.java
+++ src/firsttest/FirstTest.java
@@ -16,6 +16,9 @@ public class FirstTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello World");
+        
     }
     
 }
