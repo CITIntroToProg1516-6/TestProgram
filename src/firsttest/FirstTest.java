5b33fb9765402b18812d6149a1011d027e156fef
hcps-jordannd <hcps-jordannd@180-18JORDANND.hcps.internal>
2016-04-08 14:59:08 -0400

    Works

--- src/firsttest/FirstTest.java
+++ src/firsttest/FirstTest.java
@@ -4,6 +4,13 @@ public class FirstTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Ehllo EOwrlkd");
+        Thread thread = new Thread() {
+          
+            @Override
+            public void run() {
+                
+            }
+            
+        };
     }
 }
